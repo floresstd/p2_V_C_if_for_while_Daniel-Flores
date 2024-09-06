1c96c03fe4542decb69554e0350a56a082c827af
Jesús Flores <a.22308051281186@cbtis128.edu.mx>
2024-09-06 09:32:44 -0600

    Add files via upload
    Subí dos archivos

---
+++ p2_V_C_if_for_while/miscadenas.py
@@ -0,0 +1,26 @@
+nombre = "Daniel FLores"
+print(nombre[0])
+print(nombre)
+
+
+MisNovias = ["Agripina", "Anastacia", "Maria"]
+ 
+for chicas in MisNovias:
+ print(chicas)
+
+print("otro ejemplo")
+b = "Daniel Flores"
+print(b[2:5])
+
+print("a mayuscula")
+
+a = "Daniel FLores"
+
+print(a.upper())
+
+print("Concatenar")
+
+a = "Maria"
+b = "Perez"
+c = a + " " + b 
+print(c)
---
+++ p2_V_C_if_for_while/misvariables.py
@@ -0,0 +1,24 @@
+import math
+
+
+edad = 17 
+nombre = "Daniel Flores"
+
+print("tu edad es: ", edad)
+print("tu nombre es: ", nombre)
+
+print("--tipo de variables---")
+x = str(nombre)    
+y = int(3)    
+z = float(7)
+
+
+print(x)  
+print(y)
+print(z)
+
+print("--Funciones matematicas ---")
+
+x = math.sqrt(64)
+
+print(x)
